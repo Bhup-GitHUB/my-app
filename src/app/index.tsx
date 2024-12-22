259dcec77109e6ec527905c6fb1e18b6d1408ba5
Bhupesh Kumar <bkumar_be23@thapar.edu>
2024-12-23 02:28:34 +0530

    learning basics

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import Greet from "../components/greet";
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+      <Greet />
+    </View>
+  );
+};
+
+export default App;
