aefd9300da4b155e27692ac0102922c0355a1199
Bhupesh Kumar <bkumar_be23@thapar.edu>
2024-12-25 04:47:43 +0530

    laenign redndering

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,11 @@
 import { FlatList, Text, View } from "react-native";
 import Flaatlist from "../components/Flaatlist";
+import ArrayofObject from "../components/ArrayofObject";
 
 const App = () => {
   return (
     <View>
-      <Flaatlist />
+      <ArrayofObject />
     </View>
   );
 };
---
+++ src/components/ArrayofObject.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { View, FlatList, Text } from "react-native";
+
+const ArrayofObject = () => {
+  const locations = [
+    {
+      id: 1,
+      name: "London",
+      country: "UK",
+    },
+    {
+      id: 2,
+      name: "Paris",
+      country: "France",
+    },
+    {
+      id: 3,
+      name: "New York",
+      country: "USA",
+    },
+  ];
+
+  return (
+    <View>
+      <FlatList
+        keyExtractor={(item) => item.id.toString()}
+        data={locations}
+        renderItem={({ item }) => {
+          return (
+            <View>
+              <Text>{item.name}</Text>
+              <Text>{item.country}</Text>
+            </View>
+          );
+        }}
+      />
+    </View>
+  );
+};
+
+export default ArrayofObject;
